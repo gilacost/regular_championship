ff20b3349384aa8310061834e3eb750565e2b97c
gilacost <josepgiraltdlacoste@gmail.com>
2018-11-06 19:05:02 +0100

    feat: regular championship with docs and type specs

--- apps/regular_championship/lib/regular_championship/repo.ex
+++ apps/regular_championship/lib/regular_championship/repo.ex
@@ -17,14 +17,6 @@ defmodule RegularChampionship.Repo do
   This is used to start this Genserver as part of a supervision tree.
 
   Once the server is started, the `init/1` function of the given module is called with args as its arguments to initialize the server. To ensure a synchronized start-up procedure, this function does not return until `init/1` has returned.
-
-  ## Examples
-      iex(1)> RegularChampionship.Repo.start_link
-      {:error, {:already_started, ...}}
-
-      iex(1)> RegularChampionship.Repo.start_link
-      {:error, {:already_started, ...}}
-
   """
   def start_link() do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
@@ -34,7 +26,6 @@ defmodule RegularChampionship.Repo do
   Parses the stream with CSV and maps each row as MATCH struct
   and ProtoMatch Message.
   """
-
   @impl true
   def init(_) do
     struct_list =
@@ -46,12 +37,63 @@ defmodule RegularChampionship.Repo do
     {:ok, struct_list}
   end
 
+  # Client
   @doc """
-  Fetches the match list for a season and division and generates a list of structs
-  that will be used to print the json output.
+  Fetches the rows for a season and division and generates a list of
+  Match structs.
+
+  ## Example
+      iex> RegularChampionship.Repo.results(["SP1", "201617"]) |> List.first()
+      %RegularChampionship.Result{
+        away_team: "Eibar",
+        date: "19/08/2016",
+        div: "SP1",
+        ftag: 1,
+        fthg: 2,
+        ftr: "H",
+        home_team: "La Coruna",
+        htag: 0,
+        hthg: 0,
+        htr: "D",
+        id: 1,
+        season: 201617
+      }
   """
+  @type division() :: String.t()
+  @type season() :: String.t()
+  @type match() :: %Result{}
+
+  @spec results(division(), season()) :: list(match())
+  def results(division, season) do
+    GenServer.call(__MODULE__, {:results, [division, season]})
+  end
+
+  @doc """
+  Returns a list of list that contains two items. The first Item is
+  the league and the second is the season.
+
+  ## Example
+
+      iex> RegularChampionship.Repo.league_season_pairs()
+      [
+        ["SP1", 201617],
+        ["SP1", 201516],
+        ["SP2", 201617],
+        ["SP2", 201516],
+        ["E0", 201617],
+        ["D1", 201617]
+      ]
+  """
+  @spec league_season_pairs() :: list(list(string))
+  def league_season_pairs() do
+    GenServer.call(__MODULE__, {:league_season_pair})
+  end
+
+  # Server(callbacks)
+
+  @doc false
   @impl true
-  def handle_call({:league_season_pair, [division, season]}, _, struct_list) do
+  def handle_call({:results, [division, season]}, _, struct_list) do
     league_season_pair =
       Enum.filter(struct_list, fn
         %Result{div: d, season: s} ->
@@ -64,18 +106,14 @@ defmodule RegularChampionship.Repo do
     {:reply, league_season_pair, struct_list}
   end
 
-  @doc """
-  Returns a list with all the unique values of season or division
-  """
   @impl true
-  def handle_call({:single_values, column}, _, struct_list)
-      when column in [:div, :season] do
+  def handle_call({:league_season_pair}, _, struct_list) do
     keys_list =
       struct_list
       |> List.pop_at(0)
       |> elem(1)
-      |> Enum.group_by(&Map.get(&1, column))
-      |> Map.keys()
+      |> Enum.map(&[&1.div, "#{&1.season}"])
+      |> Enum.uniq()
 
     {:reply, keys_list, struct_list}
   end
