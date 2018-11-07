305f5b7846a73b900875c98f422ffd4069770b02
gilacost <josepgiraltdlacoste@gmail.com>
2018-11-07 12:07:29 +0100

    feat: trying to trigger hook

--- apps/api/test/api_test.exs
+++ apps/api/test/api_test.exs
@@ -2,7 +2,7 @@ defmodule ApiTest do
   use ExUnit.Case
   doctest Api
 
-  test "greets the world" do
-    assert Api.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Api.hello() == :world
+  # end
 end
