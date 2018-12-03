9c321bc3484ff6be5be519e23f635be7a116b6a1
gilacost <josepgiraltdlacoste@gmail.com>
2018-12-03 17:41:41 +0000

    feat: paginate results, adapt proto message

--- apps/api/lib/protocols/result_list.ex
+++ apps/api/lib/protocols/result_list.ex
@@ -4,13 +4,27 @@ alias RegularChampionship.Messages.ResultList, as: ResultListProto
 
 # implements specific encoding for a ResultList struct
 defimpl Api.Protobuf, for: ResultList do
-  def encode(%ResultList{data: data}) do
+  def encode(%ResultList{
+        data: %Scrivener.Page{
+          entries: entries,
+          page_number: page_number,
+          page_size: page_size,
+          total_entries: total_entries,
+          total_pages: total_pagees
+        }
+      }) do
     result_list =
-      data
+      entries
       |> Enum.map(&Map.from_struct/1)
       |> Enum.map(&ResultProto.new/1)
 
-    %{data: result_list}
+    %{
+      data: result_list,
+      page_number: page_number,
+      page_size: page_size,
+      total_entries: total_entries,
+      total_pages: total_pagees
+    }
     |> ResultListProto.new()
     |> ResultListProto.encode()
   end
